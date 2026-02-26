f7f4d13222e97b55fd557df599f925cdcfc95760
Nikhil chahar <nikhil.chahar_cs23@gla.ac.in>
2026-02-26 23:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (63.13%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSteps(String s) {
+        int st =0;
+        int carry = 0;
+        for(int i=s.length()-1;i>0;i--){
+            int bit = (s.charAt(i)-'0')+carry;
+
+            if(bit == 1){
+                st+=2;
+                carry=1;
+            }
+            else{
+                st++;
+            }
+        }
+        return st + carry;
+    }
+}
