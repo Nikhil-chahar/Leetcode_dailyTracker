dbf11eab8e41ec1738db50a4417704cb8be2c97c
Nikhil chahar <nikhil.chahar_cs23@gla.ac.in>
2026-03-19 02:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (57.12%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -14,39 +14,18 @@
  * }
  */
 class Solution {
-    List<Integer> ll = new ArrayList<>();
-    int suff[];
     public TreeNode convertBST(TreeNode root) {
-        if(root == null) return root;
-        inorder(root);
-        suf();
-        make(root);
-        return root;
+        return post(root);
     }
-    public void make(TreeNode root){
-        if(root == null) return;
-        root.val = suff[find(root.val)];
-        make(root.left);
-        make(root.right);
-    }
-    public int find(int val){
-        return ll.indexOf(val);
-    }
-    public void suf(){
-        suff = new int[ll.size()];
-        suff[ll.size()-1] = ll.get(ll.size()-1);
+    int suff=0;
+    public TreeNode post(TreeNode root){
+        if(root == null) return null;
+        post(root.right);
 
-        for(int i=ll.size()-2;i>=0;i--){
-            suff[i] = suff[i+1] + ll.get(i);
-        }
-        for(int i=0;i<ll.size();i++){
-            System.out.println(suff[i] +" ");
-        }
-    }
-    public void inorder(TreeNode root){
-        if(root == null) return;
-        inorder(root.left);
-        ll.add(root.val);
-        inorder(root.right);
+        root.val += suff;
+        suff = root.val;
+        post(root.left);
+
+        return root;
     }
 }
