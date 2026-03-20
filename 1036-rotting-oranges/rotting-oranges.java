025525c8932ba79960365b5483daa5d811c1819f
Nikhil chahar <nikhil.chahar_cs23@gla.ac.in>
2026-03-20 22:04:44 +0530

    Time: 2 ms (90.45%) | Memory: 44 MB (80.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -4,10 +4,13 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
                     q.add(new int[]{i,j});
+                }else if(grid[i][j] == 1){
+                    fresh++;
                 }
             }
         }
@@ -31,19 +34,20 @@ class Solution {
                         grid[nr][nc] = 2;
                         sp =true;
                         q.add(new int[]{nr,nc});
+                        fresh--;
                     }
                 }
             }
             if(sp) time++;
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-        return time;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(grid[i][j] == 1){
+        //             return -1;
+        //         }
+        //     }
+        // }
+        return fresh >0 ? -1 : time;
     }
 }
