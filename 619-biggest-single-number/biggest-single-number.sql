a5b8262f6d2999916231d7d52f5b319e0342c478
Nikhil chahar <nikhil.chahar_cs23@gla.ac.in>
2026-06-27 01:24:26 +0530

    Time: 457 ms (41.30%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select max(num) as num from
-    (select distinct num
+    (select distinct num,count(num) as cnt
     from mynumbers
     group by num
-    having count(*)=1) as t
+    having cnt=1) as t
