8be97a51e214d6d4d20a42b45e68b5cffacfe418
Nikhil chahar <nikhil.chahar_cs23@gla.ac.in>
2026-08-10 14:48:58 +0530

    Time: 1 ms (93.64%) | Memory: 43.4 MB (39.37%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,26 +1,56 @@
 class Solution {
+    int[][] dp;
+
     public boolean isInterleave(String s1, String s2, String s3) {
-        int n = s1.length();
-        int m = s2.length();
-        if(n+m != s3.length()){
+
+        if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
-        boolean dp[][] = new boolean[n+1][m+1];
-        dp[0][0] = true;
 
-        for(int i=1;i<=n;i++){
-            dp[i][0] = dp[i-1][0] &&(s1.charAt(i-1) == s3.charAt(i-1));       
-        }
-        for(int j=1;j<=m;j++){
-            dp[0][j] = dp[0][j-1] &&(s2.charAt(j-1) == s3.charAt(j-1));       
+        int n = s1.length();
+        int m = s2.length();
+
+        dp = new int[n + 1][m + 1];
+
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
         }
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                dp[i][j] = (dp[i-1][j] &&(s1.charAt(i-1) == s3.charAt(i+j-1)) ||
-                                (dp[i][j-1] &&(s2.charAt(j-1) == s3.charAt(i+j-1))));  
-            }     
+        // return solve(s1, s2, s3, 0, 0);
+        return solve(s1,s2,s3,0,0);
+    }
+
+    public boolean solve(String s1, String s2, String s3, int i, int j) {
+
+        // Both strings completely used
+        if (i == s1.length() && j == s2.length()) {
+            return true;
         }
-        return dp[n][m];
+
+        // Already calculated
+        if (dp[i][j] != -1) {
+            return dp[i][j] == 1;
+        }
+
+        int k = i + j;
+
+        boolean first = false;
+        boolean second = false;
+
+        // Take character from s1
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            first = solve(s1, s2, s3, i + 1, j);
+        }
+
+        // Take character from s2
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            second = solve(s1, s2, s3, i, j + 1);
+        }
+
+        boolean result = first || second;
+
+        dp[i][j] = result ? 1 : 0;
+
+        return result;
     }
 }
