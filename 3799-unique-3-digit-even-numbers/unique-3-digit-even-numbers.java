9657965e73ff010803869bb3088186910e45c201
Nikhil chahar <nikhil.chahar_cs23@gla.ac.in>
2026-09-12 00:40:18 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (88.67%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,35 @@
+class Solution { 
+    public int totalNumbers(int[] digits) { 
+        int freq[] = new int[10]; 
+ 
+        for(int v : digits){ 
+            freq[v]++; 
+        } 
+
+        int ans = 0;
+
+        for(int i = 1; i <= 9; i++){          // hundreds digit
+            if(freq[i] == 0) continue;
+
+            freq[i]--;
+
+            for(int j = 0; j <= 9; j++){      // tens digit
+                if(freq[j] == 0) continue;
+
+                freq[j]--;
+
+                for(int k = 0; k <= 8; k += 2){ // units digit
+                    if(freq[k] > 0){
+                        ans++;
+                    }
+                }
+
+                freq[j]++;
+            }
+
+            freq[i]++;
+        }
+
+        return ans;
+    }
+}
